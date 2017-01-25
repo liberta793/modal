e7ce8c5409abe74b3eefff36cef19dcc2a031ec7
murata-yuka <murata-yuka@9105-109520.local>
2017-01-25 21:38:34 +0900

    簡単にpagesに反映できるように対応

--- modal.js
+++ modal.js
@@ -2,18 +2,27 @@ $(function(){
 
 // モーダルウィンドウが開くときの処理	
 $(".modal_1").click(function(){
-	$("#modal01").fadeIn();
-	$(".overlay").fadeIn();
-	$(this).addClass("open");
+	$(".overlay").velocity("fadeIn",{
+		duration:350,
+		complete:function(){
+			$("#modal01").velocity("fadeIn",{
+				duration:350});
+		}
+	});
 	return false;
 });
 
 // モーダルウィンドウが閉じるときの処理	
 $(".modalclose").click(function(){
-	$("#modal01").fadeOut();
-	$(".overlay").fadeOut();
+	$("#modal01").velocity("fadeOut",{
+		duration:350,
+		complete:function(){
+			$(".overlay").velocity("fadeOut",{
+				duration:350});
+		}
+	});
 	return false;
-});  
+});
 
 
 });
